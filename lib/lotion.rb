f8b601ebd353e59611a43e3879627aea25201f3e
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-05-09 09:22:07 -0700

    Add Dependencies helper to find dependencies relative to the calling file

--- lib/lotion.rb
+++ lib/lotion.rb
@@ -1,10 +1,17 @@
 require 'lotion/version'
 require 'codependency'
+require 'pathname'
 
 module Lotion
 
   SOURCE = Dir[ File.expand_path( File.join( __FILE__, '../lotion/**/*.rb' ) ) ]
   FILES  = Codependency::Graph.new( SOURCE ).files.freeze
+  PATHS  = FILES.map { |f| Pathname.new( f ) }.freeze
+
+  def self::Dependencies( file )
+    path = Pathname.new( file ).dirname
+    PATHS.map { |p| p.relative_path_from( path ).to_s }
+  end
 
   autoload :Application, 'lotion/application'
   autoload :Concern,     'lotion/concern'
