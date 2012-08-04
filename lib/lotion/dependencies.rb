0d6dae85ae289083a557fbf4f40840bcc011539e
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-08-03 22:34:55 -0700

    RubyMotion needs relative paths for dependencies

--- lib/lotion/dependencies.rb
+++ lib/lotion/dependencies.rb
@@ -4,9 +4,13 @@ require 'pathname'
 module Lotion
 
   def self::Dependencies( file )
-    start = File.join File.dirname( __FILE__ ), 'container.rb'
-    graph = Codependency::Graph.new start
-    graph.files
+    @dependencies ||= begin
+      start = File.expand_path File.join __FILE__, '../container.rb'
+      graph = Codependency::Graph.new start
+      app   = Pathname.new( file ).dirname
+
+      graph.files.map { |f| Pathname.new( f ).relative_path_from( app ).to_s }
+    end
   end
 end
 
