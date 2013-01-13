645c56e6d31b09c6d35b2a70a8b976dfdf910d39
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2013-01-13 01:10:56 -0800

    Add text field keyboard control options

--- lib/lotion/view/ui_text_field.rb
+++ lib/lotion/view/ui_text_field.rb
@@ -11,17 +11,44 @@ module Lotion
         view.placeholder = value
       end
 
+      def clear_button_mode( value )
+        view.setClearButtonMode value
+      end
+
+      # UITextInputTraits Protocol
+
+      def autocapitalization_type( value )
+        view.autocapitalizationType = value
+      end
+
       def autocorrection_type( value )
         view.autocorrectionType = value
       end
 
-      def clear_button_mode( value )
-        view.setClearButtonMode value
+      def enables_return_key_automatically( value )
+        view.enablesReturnKeyAutomatically = value
+      end
+
+      def keyboard_appearance( value )
+        view.keyboardAppearance = value
+      end
+
+      def keyboard_type( value )
+        view.keyboardType = value
+      end
+
+      def return_key_type( value )
+        view.returnKeyType = value
       end
 
       def secure_text_entry( value )
         view.secureTextEntry = value
       end
+
+      def spell_checking_type( value )
+        view.spellCheckingType = value
+      end
+
     end
   end
 end
