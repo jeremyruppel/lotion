5adbb2f21f1f844089b8046881ee61ffb85da130
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2013-01-15 07:41:21 -0800

    Spec out named array

--- spec/lotion/core_ext/ui_kit/ui_table_view_data_source_spec.rb
+++ spec/lotion/core_ext/ui_kit/ui_table_view_data_source_spec.rb
@@ -1,3 +1,22 @@
+describe Lotion::NamedArray do
+  before do
+    @subject = described_class.new 'foo'
+    @subject << [ 1, 2, 3 ]
+    @subject << [ 1, 2 ]
+  end
+
+  it 'has a #name' do
+    @subject.name.should == 'foo'
+  end
+  it 'is an array' do
+    @subject.is_a?( Array ).should == true
+  end
+  it 'can lookup by an NSIndexPath' do
+    indexPath = NSIndexPath.indexPathForRow 2, inSection:0
+    @subject[ indexPath ].should == 3
+  end
+end
+
 describe Lotion::UITableViewDataSource do
 
   subject do
