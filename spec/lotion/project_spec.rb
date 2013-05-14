dd5dd51eee8b6c2e5e368cb49814abc5a1864734
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2013-05-14 13:20:20 -0700

    Add a placeholder project spec

---
+++ spec/lotion/project_spec.rb
@@ -0,0 +1,5 @@
+# This file exists just so guard doesn't freak out when project.rb
+# is modified. Since this file should only be included in a Rakefile,
+# there is nothing to test in the RubyMotion runtime.
+describe 'Lotion::Project' do
+end
