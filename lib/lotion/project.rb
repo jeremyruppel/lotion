4b766a80b03564fe7bc0f05b5e845af5857c3d2d
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2013-06-12 12:12:27 -0700

    It merged! Woot!

--- lib/lotion/project.rb
+++ lib/lotion/project.rb
@@ -1,30 +1,8 @@
 require 'lotion'
 
-module Lotion
-  module Project
-
-    # FIXME remove once https://github.com/HipByte/RubyMotion/pull/90 merges
-    def files_dependencies(deps_hash)
-      res_path = lambda do |x|
-        path = /^\.{0,2}\//.match(x) ? x : File.join('.', x)
-        unless @files.flatten.include?(path)
-          App.fail "Can't resolve dependency `#{path}'"
-        end
-        path
-      end
-      deps_hash.each do |path, deps|
-        deps = [deps] unless deps.is_a?(Array)
-        @dependencies[res_path.call(path)] = deps.map(&res_path)
-      end
-    end
-  end
-end
-
 Lotion.require do |graph|
 
   Motion::Project::App.setup do |app|
-    app.extend Lotion::Project
-
     # Add the app dir to the search path
     graph.path << './app'
 
